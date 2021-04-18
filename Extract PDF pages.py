412c9e7fef1ca8fca1079e8ed611a60ebd8f898c
Jason <67337624+Jason897571@users.noreply.github.com>
2021-04-18 15:41:19 -0400

    Add Annotation

--- Extract PDF pages.py
+++ Extract PDF pages.py
@@ -2,20 +2,22 @@ import PyPDF2
 import os
 
 
-path1 = r"C:\Users\QB333LP\OneDrive - EY\Desktop\Amaranth\2020\5472"
+path1 = "C:\pdf" # path for input PDF
 
-file_list = os.listdir(path1)
+file_list = os.listdir(path1) # To get the name list of PDF under path 1
+
 
 for name in file_list:
-    path2 = r"C:\Users\QB333LP\OneDrive - EY\Desktop\Amaranth\2020\5472\{name}".format(name=name)
-    path3 = r"C:\Users\QB333LP\OneDrive - EY\Desktop\Amaranth\2020\signature\{name}".format(name=name)
-    path3 = path3.replace("_5472"," signature page")
-    file = PyPDF2.PdfFileReader(path2)
+    path2 = r"C:\pdf\{name}".format(name=name) # path for each pdf file
+    path3 = r"C:\pdf\output\{name}".format(name=name) # path for output PDF
+    path3 = path3.replace("_5472"," signature page") # rename the output PDF
+    
+    
+    file = PyPDF2.PdfFileReader(path2) # to read each pdf
+    pdfWriter = PyPDF2.PdfFileWriter() # to lunch pdfWriter 
+    pdfWriter.addPage(file.getPage(0)) # to get the first page of each pdf
 
-    pdfWriter = PyPDF2.PdfFileWriter()
-
-    pdfWriter.addPage(file.getPage(0))
-
-    with open(path3,"wb") as f:
-        pdfWriter.write(f)
+    
+    with open(path3,"wb") as f: # to save pdf in under path 3
+        pdfWriter.write(f) 
         f.close()
